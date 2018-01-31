3d5fcb08b65fc884770a2c9e74c0da7e88a90b1e
lukemarak <lukemarak@gmail.com>
2018-01-31 16:26:57 +0530

    3rd commit

--- yalni/records/admin.py
+++ yalni/records/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Assembled
+
+class AssembledAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'chasis', 'motherboard', 'configuration', 'entered']
+admin.site.register(Assembled, AssembledAdmin)
